168f288c2840dfbe84aa2881a8738301e59beda7
pragaas <beppo.india@gmail.com>
2017-01-22 13:41:29 +0530

    test changes

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
 var express = require('express');
 var app = express();
-var MongoClient = require('bluebird').promisifyAll(require('mongodb')).MongoClient;
-//var cors = require('cors');
-//var http = require('http').createServer(app);
+var Promise = require('bluebird');
+var MongoClient = Promise.promisifyAll(require('mongodb')).MongoClient;
 var port = 80 ;
-
-//app.use(cors);
 
 var url = 'mongodb://localhost/tevotedDB';
 var timerData = [];
 
-/*function getData(){
-MongoClient.connect(url, function (err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        var cursor = db.collection('timerData').find();
-        cursor.each(function(err, doc){
-          if(doc !== null){
-              timerData.push(doc);
-          }
-        });
-
-        db.close();
-    }
-});
-}
-*/
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', req.headers.origin);
@@ -36,21 +16,24 @@ app.use(function(req, res, next) {
 });
 
 app.get("/",function(req,res){
-MongoClient.connectAsync(url)
- .then(function (err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-	db.collection('timerData').findAsync({})
-	.then(function(cursor){
-		return  cursor.toArrayAsync();
-	})
-	.then(function(recordsArray){
-		res.send(recordsArray);
-	});
-        db.close();
-   }
- }); // then MongoClient
-}); // app.get
+    MongoClient.connectAsync(url)
+        .then(function(db){
+            return db.collection('timerData').findAsync({})
+        })
+        .then(function(cursor){
+            return cursor.toArrayAsync();
+        })
+        .then(function(content){
+            res.status(200).json(content);
+        })
+        .catch(function(err) {
+            throw err;
+        })
+        .finally(){
+            db.close();
+        }
+    //});
+});
+
 app.listen(port);
 
